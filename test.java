b695b7e647a7712821129450ab105f4488ab5f6c
Ajeyarajap <Ajeyarajap@users.noreply.github.com>
2015-08-04 11:19:10 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,19 @@
+@ProcessAction(name = "submitSample")
+    public void submitSample(ActionRequest request, ActionResponse response) throws IOException, PortletException {
+     SampleCodeSubmitInfo sampleInfo = new SampleCodeSubmitInfo();
+        boolean hasSnippet = true;
+        for (int i = 0; hasSnippet; i++) {
+            String input = new StringBuilder("sample-file-name_").append(i).toString();
+            String fileName = PortletUtil.getParameter(request, input);
+            if (!StringUtils.EMPTY.equals(fileName)) {
+                input = new StringBuilder("sample-snippet-content_").append(i).toString();
+                String fileContent = PortletUtil.getParameter(request, input);
+                SampleCodeFile sampleFile = new SampleCodeFile();
+                sampleFile.setName(fileName);
+                sampleFile.setContent(fileContent);
+                sampleInfo.addFile(sampleFile);
+            } else {
+                hasSnippet = false;
+            }
+        }
+    }
